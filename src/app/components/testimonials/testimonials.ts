cf7b6d090bd8a53656adb60612181e6139f19b9b
jihenJN <jihenjihen23@gmail.com>
2026-03-23 21:46:59 +0100

    feat : add testimonials

--- src/app/components/testimonials/testimonials.ts
+++ src/app/components/testimonials/testimonials.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CONTENT } from '../../content';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-testimonials',
-  imports: [],
+  imports: [ NgForOf],
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.css',
 })
 export class Testimonials {
+  content = CONTENT;
 
 }
