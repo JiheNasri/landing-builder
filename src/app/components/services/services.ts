89fc4169496ee9c56ae2ca60df2d49d993031db9
jihenJN <jihenjihen23@gmail.com>
2026-03-23 21:29:50 +0100

    feat : add services

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CONTENT } from '../../content';
+import { NgForOf, NgIf } from '@angular/common';
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [NgForOf, NgIf],
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
 export class Services {
-
+content = CONTENT;
 }
